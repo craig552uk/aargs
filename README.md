c7a5e5c30e3e859bd55aa33962925f37d9d32ffa
Craig Russell <craig.russell@synety.com>
2016-07-11 13:11:18 +0000

    Added support for -a switches

--- README.md
+++ README.md
@@ -7,11 +7,13 @@ Author: Craig Russell <craig@craig-russell.co.uk>
 Parse command line arguments into an object e.g.
 
 ```javascript
-> node /apth/to/script.js --foo --bar=baz /path/to/file
+> node script.js -a -b Bzz --foo --bar=baz /path/to/file
 
 { '0': '/usr/bin/nodejs',
-  '1': '/var/www/nodejs/dummyapp/app.js',
+  '1': '/home/user/script.js',
   '2': '/path/to/file',
+  a: true,
+  b: 'Bzz',
   foo: true,
   bar: 'baz' }
 ```
--- aargs.js
+++ aargs.js
@@ -1,16 +1,33 @@
-var n = 0, a;
+var n = 0, i = 0, arg, arr, key, val;
 module.exports = {};
 
-// Join short flags with '='
-var args = process.argv.join(' ').replace(/ -([a-zA-Z]) ([a-zA-Z0-9])/g, " -$1=$2");
+for(var i=0, l=process.argv.length; i<l; i++){
+    arg = process.argv[i];
 
-args.split(' ').forEach((v,k) => {
-    if(v.match(/^-{1,2}/)){
-        a = v.replace(/^-*/,'').split('=');
-        k = a[0];
-        v = a[1] || true;
-    }else{
-        k = n++;
+    // Skip null values
+    if(arg===null) continue;
+
+    // --foo
+    // --foo=bar
+    if(arg.match(/^--/)){
+        arr = arg.replace(/^-*/,'').split('=');
+        module.exports[arr[0]] = arr[1] || true;
+        continue
     }
-    module.exports[k] = v;
-});
+
+    // -a
+    // -a value
+    if(arg.match(/^-[^\-]/)){
+        key = arg.replace(/^-/,'');
+        if(process.argv[i+1].match(/^[^\-]/)){
+            module.exports[key] = process.argv[i+1];
+            process.argv[i+1]   = null;
+        }else{
+            module.exports[key] = true;
+        }
+        continue;
+    }
+
+    
+    module.exports[n++] = arg;
+}
